66c8f03e7687aa6128c769dc3b16e3c4cba45f67
itsmearion <bilqiss0893@gmail.com>
2025-04-23 20:13:50 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# config.py
+API_ID = 12345678            # <- ganti sama API ID kamu
+API_HASH = "your_api_hash"    # <- ganti sama API HASH kamu
+BOT_TOKEN = "your_bot_token"  # <- ganti sama BOT TOKEN kamu
