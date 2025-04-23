1225c7229bea8df712a1418424ea90fa64a4cd0f
itsmearion <bilqiss0893@gmail.com>
2025-04-23 21:40:37 +0700

    Update error_handler.py

--- utils/error_handler.py
+++ utils/error_handler.py
@@ -1,6 +1,9 @@
 import logging
+from pyrogram import filters
+from pyrogram.handlers import MessageHandler
 
-logger = logging.getLogger("GundaBot")
-
-async def on_error(client, update, error):
-    logger.error(f"Error terjadi: {error}")
+async def on_error(client, message):
+    try:
+        logging.error(f"Error on message: {message}")
+    except Exception as e:
+        logging.error(f"Error in error_handler: {e}")
