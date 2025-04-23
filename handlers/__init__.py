21655c0cef3a556fba4ba701b0f58e42cfbd0c90
itsmearion <bilqiss0893@gmail.com>
2025-04-23 21:31:07 +0700

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,6 +1,10 @@
 from .start_handler import start_handler
 from .format_handler import format_handler
+from .user_to_admin import user_to_admin_handler
+from .admin_to_user import admin_to_user_handler
 
 def register_handlers(app):
     start_handler(app)
     format_handler(app)
+    user_to_admin_handler(app)
+    admin_to_user_handler(app)
